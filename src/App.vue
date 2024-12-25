1b72ff31b88a26d49cc210ea553b81c4d0d0173d
SH <aotushi@hotmail.com>
2024-12-25 11:48:58 +0800

    qita

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 let a = 1
-console.log('a', a)
+console.log('ab', a)
 </script>
 
 <template>

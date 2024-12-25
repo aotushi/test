aa234efbf4953aa50c3f79e727fda068198dc308
SH <aotushi@hotmail.com>
2024-12-25 13:41:56 +0800

    test: :white_check_mark: sdf

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 let a = 1
-console.log('abcd', a)
+console.log('abcdf', a)
 </script>
 
 <template>
